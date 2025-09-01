ea11d728bcf84ea3430196f9c631272a50fec0bc
agrawalkristi242007-sys <agrawalkristi242007@gmail.com>
2025-09-01 23:16:33 +0530

    Experiment 1

---
+++ Experiment 1.3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+printf("Name-krishtiagrawal\n SAP ID-590025883\ncourse-BSC CS\BATCH-B1\n");
+printf("\n--------------------------------\n");
+	int a,b,sum;
+	printf("the first number of a and b");
+	scanf("%d" "%d", &a, &b);
+	sum=a+b;
+	printf("%d",sum);
+	return 0;
+}
